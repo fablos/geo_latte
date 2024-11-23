85d833d568a696fd82d75c48450632648e98e57f
fabris <fabrizio.loscudo@unical.it>
2024-11-23 15:46:38 +0100

    minor fixes

--- icra/utils.py
+++ icra/utils.py
@@ -6,51 +6,6 @@ from torch.distributions.kl import kl_divergence as KL
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-class DensityMetric(nn.Module):
-    def __init__(self, data, sigma):
-        super(DensityMetric, self).__init__()
-        self.data = data
-        self.sigma = sigma
-
-    def density(self, x):
-        """
-        Evaluate a kernel density estimate at x. It builds a gaussian around each point x
-
-        Parameters:
-        x: points where the density is evaluated. Dimensions (num_points)x(data_dim)
-        """
-        N, D = x.shape
-        M, _ = self.data.shape
-        sigma2 = self.sigma**2
-        normalization = (2 * 3.14159)**(D/2) * self.sigma**D  # scalar
-        distances = torch.cdist(x, self.data)  # NxM
-        K = torch.exp(-0.5 * distances**2 / sigma2) / normalization  # NxM
-        p = torch.sum(K, dim=1)  # N
-        return p
-
-    def curve_energy(self, C):
-        """
-        Compute the length of a curve when passed through the decoder mean.
-
-        Parameters:
-        C: points along a curve. Dimensions (batch)x(num_points_along_curve)x(data_dim)
-        """
-        pass # XXX: remove me!
-
-        if C.dim() == 2:
-            C = C.unsqueeze(0)
-        B, lenT, D = C.shape
-        CC = C.reshape(-1, D)
-        p = self.density(CC)  # (B*lenT)
-
-        metric = 1 / (p + 1e-4).reshape(B, lenT)  # (B)x(lenT)
-        avg_metric = 0.5 * metric[:, 1:] + 0.5 * metric[:, :-1]  # (B)x(lenT-1)
-
-        delta = C[:, 1:] - C[:, :-1]  # (B)x(lenT-1)x(D)
-        energy = torch.sum(torch.sum(delta ** 2, dim=2) * avg_metric, dim=1)  # (B)
-
-        return energy
-
 # I can use spline from the StochMan library
 class Poly2(nn.Module):
     def __init__(self, x0, x1, N, device='cpu'):

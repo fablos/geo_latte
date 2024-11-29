6d76ffef954b7c9a1624fa17603a9ea4cd0a39b7
Fabrizio Lo Scudo <fabrizio.loscudo@unical.it>
2024-11-29 15:03:49 +0100

    examples

--- example/circular_utils.py
+++ example/circular_utils.py
@@ -86,9 +86,50 @@ def get_decoder(z_dim=2, out_dim=100):
     return decoder_net
 
 
-def generate_grid(v=2, grid_size=100, device='cpu'):
-    x_lin = torch.linspace(-v, v, grid_size).to(device)
-    y_lin = torch.linspace(-v, v, grid_size).to(device)
+def generate_grid(bound=2, grid_size=100, device='cpu'):
+    x_lin = torch.linspace(-bound, bound, grid_size).to(device)
+    y_lin = torch.linspace(-bound, bound, grid_size).to(device)
     x_grid, y_grid = torch.meshgrid(x_lin, y_lin, indexing='ij')
     grid = torch.stack((x_grid, y_grid), dim=-1)
     return grid, x_lin.cpu(), y_lin.cpu()
+
+
+class DensityMetric(nn.Module):
+    def __init__(self, data, sigma):
+        super(DensityMetric, self).__init__()
+        self.data = data
+        self.sigma = sigma
+
+    def density(self, x):
+        """
+        Evaluate a kernel density estimate at x.
+
+        Parameters:
+        x: points where the density is evaluated. Dimensions (num_points)x(data_dim)
+        """
+        N, D = x.shape
+        M, _ = self.data.shape
+        sigma2 = self.sigma**2
+        normalization = (2 * 3.14159)**(D/2) * self.sigma**D  # scalar
+        distances = torch.cdist(x, self.data)  # NxM
+        K = torch.exp(-0.5 * distances**2 / sigma2) / normalization  # NxM
+        p = torch.sum(K, dim=1)  # N
+        return p
+
+    def curve_energy(self, C):
+        """
+        Compute the length of a curve
+
+        Parameters:
+        C: points along a curve. Dimensions (batch)x(num_points_along_curve)x(data_dim)
+        """
+        if C.dim() == 2:
+            C = C.unsqueeze(0)
+        B, lenT, D = C.shape
+        CC = C.reshape(-1, D)
+        p = self.density(CC)  # (B*lenT)
+        metric = 1 / (p + 1e-4).reshape(B, lenT)  # (B)x(lenT)
+        avg_metric = 0.5 * metric[:, 1:] + 0.5 * metric[:, :-1]  # (B)x(lenT-1)
+        delta = C[:, 1:] - C[:, :-1]  # (B)x(lenT-1)x(D)
+        energy = torch.sum(torch.sum(delta**2, dim=2) * avg_metric, dim=1)  # (B)
+        return energy
